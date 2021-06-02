755af6fe7224f161dd586dc3f93d03e9bed44d2c
ShubhankSinghal <shubhank.singhal98@gmail.com>
2021-06-02 23:27:58 +0530

    postman part

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order("due_date").map { |todo| todo.to_pleasant_string }
   end
@@ -8,5 +10,13 @@ class TodosController < ApplicationController
     id = params[:id]
     todo = Todo.find(id)
     render plain: todo.to_pleasant_string
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    response_text = "Hey! Your new todo is created with id #{new_todo.id}"
+    render plain: response_text
   end
 end
