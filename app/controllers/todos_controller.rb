e25bc2cf1177e2f510f7e0889226e89bd453b65f
ShubhankSinghal <shubhank.singhal98@gmail.com>
2021-06-13 03:16:01 +0530

    add todo functionality

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,9 +17,12 @@ class TodosController < ApplicationController
   def create
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
-    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    response_text = "Hey! Your new todo is created with id #{new_todo.id}"
-    render plain: response_text
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false
+    )
+    redirect_to todos_path
   end
 
   def update
